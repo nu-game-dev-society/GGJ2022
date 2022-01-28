9abe710e523e74f9f4207de96a4921b5becc20d6
Andrew N's Laptop <a.nattress@gmail.com>
2022-01-28 19:02:33 +0000

    Player Controller Started

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController cc;
+    public Transform cam;
+    public float speed;
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    float mouseY;
+    void LateUpdate()
+    {
+        Vector3 dir = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal")).normalized;
+        cc.Move(speed * Time.deltaTime * dir);
+
+        transform.Rotate(new Vector3(0, Input.GetAxisRaw("Mouse X") * Time.deltaTime, 0));
+        mouseY += Input.GetAxis("Mouse Y") * Time.deltaTime;
+        mouseY = Mathf.Clamp(mouseY, -60, 60);
+        cam.localEulerAngles = new Vector3(-mouseY, 0, 0);
+    }
+}
