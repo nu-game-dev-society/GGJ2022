d6b55da02b0ecdc99971b479bb3887899f598788
rtm516 <ryantmilner@hotmail.co.uk>
2022-01-29 13:06:20 +0000

    Book randomisation

--- Assets/Scripts/BookController.cs
+++ Assets/Scripts/BookController.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class BookController : MonoBehaviour, IInteractable
 {
-    public IEnumerable<Material> GetMaterials() => gameObject.GetComponentsInChildren<Renderer>().SelectMany(renderer => renderer.materials);
+    [SerializeField]
+    private TextMeshPro cover;
+    [SerializeField]
+    private TextMeshPro inside;
+    [SerializeField]
+    private TextMeshPro bind;
 
+    [SerializeField]
+    private SkinnedMeshRenderer meshRenderer;
+
+    [SerializeField]
+    private Material[] materials;
+
+    public IEnumerable<Material> GetMaterials() => gameObject.GetComponentsInChildren<Renderer>().SelectMany(renderer => renderer.materials);
 
     public void Interact(PlayerController player)
     {
         gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        cover.text = BookData.RandomTitle() + "\n\n\n\n\n\n" + BookData.RandomSubTitle();
+        meshRenderer.material = materials[Random.Range(0, materials.Length - 1)];
     }
 }
